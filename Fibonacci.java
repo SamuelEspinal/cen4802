6d5bb8aec3d83ffd671d695ba0e2249f8ac88aee
Damoganosaem <130258898+SamuelEspinal@users.noreply.github.com>
2023-09-05 19:50:04 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,15 @@
-public class Fibonacci {
+/**
+ * A class that provides methods for calculating Fibonacci numbers.
+ */
+public class FibonacciCalculator {
 
+    /**
+     * Calculates the nth term in the Fibonacci sequence.
+     *
+     * @param n The position of the term to calculate (must be a positive integer).
+     * @return The nth term in the Fibonacci sequence.
+     * @throws IllegalArgumentException if n is not a positive integer.
+     */
     public static int calculateFibonacci(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("n must be a positive integer.");
@@ -10,6 +20,11 @@ public class Fibonacci {
         }
     }
 
+    /**
+     * The main method to demonstrate the usage of the calculateFibonacci method.
+     *
+     * @param args The command-line arguments (not used).
+     */
     public static void main(String[] args) {
         int n = 10;
         int nthFibonacci = calculateFibonacci(n);
